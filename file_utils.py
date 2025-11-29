679b5868bf88dac806fa9ec9d6b9e0368ccd4675
Matsu <matsu3x9@icloud.com>
2025-11-30 06:31:47 +0800

    refactor: backup fun os.path -> pathlib

--- file_utils.py
+++ file_utils.py
@@ -1,36 +1,18 @@
 import shutil
+from pathlib import Path
 from datetime import datetime
-from os import makedirs, path
-from os.path import (
-    basename,
-    dirname,
-    expanduser,
-)
 
-from ._color_print import print_red
+from ._cprint import printr
 
 
-def backup(bak_file: str):
-    if not path.exists(bak_file):
-        if not path.islink(bak_file):
-            return
+def backup(bak_file: Path):
+    bak_dir = Path.home() / "ln39.bak"
+    bak_dir.mkdir(parents=True, exist_ok=True)
 
-    home = expanduser("~")
-    bak_dir = path.join(home, "ln39.bak")
-    makedirs(bak_dir, exist_ok=True)
+    base_name = bak_file.name
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
+    dest_path = bak_dir / f"{base_name}_{timestamp}"
 
-    base_name = basename(bak_file)
-    dest_path = path.join(bak_dir, base_name)
-
-    if path.exists(dest_path):
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        dest_path = path.join(bak_dir, f"{base_name}_{timestamp}")
-    print_red(f"[BACKUP]   Moved to backup               : {bak_file} --> {dest_path}")
+    printr(f"{bak_file} --> {dest_path}", "BACKUP")
+    # shutil move 可以跨分区
     shutil.move(bak_file, dest_path)
-
-
-def check_parent_dir(dest_path: str):
-    parent = dirname(dest_path)
-    if not path.exists(parent):
-        print_red(f"[MKDIR]    Creating missing directory    : {parent}")
-        makedirs(parent, exist_ok=True)

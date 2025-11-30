733cd8d5e73a00f77db3d6654efed00bea957178
Matsu <matsu3x9@icloud.com>
2025-11-30 21:36:27 +0800

    feat: args type: str | Path

--- file_utils.py
+++ file_utils.py
@@ -1,13 +1,15 @@
 import shutil
 from pathlib import Path
 from datetime import datetime
+from os.path import expandvars
 
 from ._cprint import printr
 
 
-def backup(bak_file: Path):
+def backup(bak_file: Path | str):
     bak_dir = Path.home() / "ln39.bak"
     bak_dir.mkdir(parents=True, exist_ok=True)
+    bak_file = Path(expandvars(str(bak_file))).expanduser()
 
     base_name = bak_file.name
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
